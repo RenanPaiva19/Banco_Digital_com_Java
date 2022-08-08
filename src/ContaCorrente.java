b5f8cc0a64cc7f2446541b75e9914401f32e19a6
RenanPaiva19 <rpaiva.1994@gmail.com>
2022-08-08 13:20:25 -0300

    transaçoes funcionando

---
+++ src/ContaCorrente.java
@@ -0,0 +1,10 @@
+
+public class ContaCorrente extends Conta {
+
+	@Override
+	public void ImprimirExtrato() {
+		System.out.println("=== Extrato Conta Corrente ===");
+		super.imprimirInfosComuns();
+	}
+
+}
---
+++ src/ContaPoupanca.java
@@ -0,0 +1,9 @@
+
+public class ContaPoupanca extends Conta {
+
+	@Override
+	public void ImprimirExtrato() {
+		System.out.println("=== Extrato Conta Poupanca ===");
+		super.imprimirInfosComuns();
+	}
+}
